37dcc0b68c71117476e89837152a2f94da27df3c
Leonard Kioi Kinyanjui <lenny.kinyanjui.lk@gmail.com>
2021-05-04 10:40:08 +0300

    flesh out tests and headers functionality

--- example/shelf_cors_headers_example.dart
+++ example/shelf_cors_headers_example.dart
@@ -1,6 +1,19 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
 import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  var handler =
+      const Pipeline().addMiddleware(corsHeaders()).addHandler(_echoRequest);
+
+  var server = await serve(handler, 'localhost', 8080);
+
+  // Enable content compression
+  server.autoCompress = true;
+
+  print('Serving at http://${server.address.host}:${server.port}');
+}
+
+Response _echoRequest(Request request) {
+  return Response.ok('Request for "${request.url}"');
 }
--- lib/src/shelf_cors_headers_base.dart
+++ lib/src/shelf_cors_headers_base.dart
@@ -1,3 +1,5 @@
+import 'package:shelf/shelf.dart';
+
 const ACCESS_CONTROL_ALLOW_ORIGIN = 'Access-Control-Allow-Origin';
 const ACCESS_CONTROL_EXPOSE_HEADERS = 'Access-Control-Expose-Headers';
 const ACCESS_CONTROL_ALLOW_CREDENTIALS = 'Access-Control-Allow-Credentials';
@@ -5,7 +7,7 @@ const ACCESS_CONTROL_ALLOW_HEADERS = 'Access-Control-Allow-Headers';
 const ACCESS_CONTROL_ALLOW_METHODS = 'Access-Control-Allow-Methods';
 const ACCESS_CONTROL_MAX_AGE = 'Access-Control-Max-Age';
 
-const defaultHeadersList = [
+const _defaultHeadersList = [
   'accept',
   'accept-encoding',
   'authorization',
@@ -15,13 +17,49 @@ const defaultHeadersList = [
   'user-agent',
 ];
 
-const defaultMethodsList = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT'];
+const _defaultMethodsList = [
+  'DELETE',
+  'GET',
+  'OPTIONS',
+  'PATCH',
+  'POST',
+  'PUT'
+];
 
-Map<String, dynamic> defaultHeaders = {
+Map<String, String> _defaultHeaders = {
   ACCESS_CONTROL_ALLOW_ORIGIN: '*',
   ACCESS_CONTROL_EXPOSE_HEADERS: '',
   ACCESS_CONTROL_ALLOW_CREDENTIALS: 'true',
-  ACCESS_CONTROL_ALLOW_HEADERS: defaultHeadersList.join(','),
-  ACCESS_CONTROL_ALLOW_METHODS: defaultMethodsList.join(','),
-  ACCESS_CONTROL_MAX_AGE: 86400,
+  ACCESS_CONTROL_ALLOW_HEADERS: _defaultHeadersList.join(','),
+  ACCESS_CONTROL_ALLOW_METHODS: _defaultMethodsList.join(','),
+  ACCESS_CONTROL_MAX_AGE: '86400',
 };
+
+Middleware corsHeaders({Map<String, String>? headers}) {
+  Response? updateRequest(Request request) {
+    var _headers = _defaultHeaders;
+
+    if (headers != null) {
+      _headers.addAll(headers);
+    }
+
+    if (request.method == 'OPTIONS') {
+      return Response.ok(null, headers: _headers);
+    }
+
+    return null;
+  }
+
+  Response updateResponse(Response response) {
+    var _headers = _defaultHeaders;
+
+    if (headers != null) {
+      _headers.addAll(headers);
+    }
+
+    return response.change(headers: _headers);
+  }
+
+  return createMiddleware(
+      requestHandler: updateRequest, responseHandler: updateResponse);
+}
